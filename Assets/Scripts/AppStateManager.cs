7402a012def73a58ec9719dea7804c34b8bca327
ReiiYuki <voraton.l@ku.th>
2017-06-12 14:16:07 +0900

    Anchor but no teset because of one holo

--- Assets/Scripts/AppStateManager.cs
+++ Assets/Scripts/AppStateManager.cs
@@ -12,11 +12,14 @@ public class AppStateManager : Singleton<AppStateManager>
     public enum AppState
     {
         Starting = 0,
+        PickingAvatar,
         WaitingForAnchor,
         WaitingForStageTransform,
-        PickingAvatar,
         Ready
     }
+
+    // The object to call to make a projectile.
+    GameObject shootHandler = null;
 
     /// <summary>
     /// Tracks the current state in the experience.
@@ -25,11 +28,38 @@ public class AppStateManager : Singleton<AppStateManager>
 
     void Start()
     {
+        // The shootHandler shoots projectiles.
+        if (GetComponent<ProjectileLauncher>() != null)
+        {
+            shootHandler = GetComponent<ProjectileLauncher>().gameObject;
+        }
+
         // We start in the 'picking avatar' mode.
         CurrentAppState = AppState.PickingAvatar;
 
-        // We start by showing the avatar picker.
+        // Spatial mapping should be disabled when we start up so as not
+        // to distract from the avatar picking.
+        SpatialMappingManager.Instance.StopObserver();
+        SpatialMappingManager.Instance.gameObject.SetActive(false);
+
+        // On device we start by showing the avatar picker.
         PlayerAvatarStore.Instance.SpawnAvatarPicker();
+    }
+
+    public void ResetStage()
+    {
+        // If we fall back to waiting for anchor, everything needed to 
+        // get us into setting the target transform state will be setup.
+        if (CurrentAppState != AppState.PickingAvatar)
+        {
+            CurrentAppState = AppState.WaitingForAnchor;
+        }
+
+        // Reset the underworld.
+        if (UnderworldBase.Instance)
+        {
+            UnderworldBase.Instance.ResetUnderworld();
+        }
     }
 
     void Update()
@@ -44,10 +74,17 @@ public class AppStateManager : Singleton<AppStateManager>
                 }
                 break;
             case AppState.WaitingForAnchor:
+                // Once the anchor is established we need to run spatial mapping for a 
+                // little while to build up some meshes.
                 if (ImportExportAnchorManager.Instance.AnchorEstablished)
                 {
                     CurrentAppState = AppState.WaitingForStageTransform;
                     GestureManager.Instance.OverrideFocusedObject = HologramPlacement.Instance.gameObject;
+
+                    SpatialMappingManager.Instance.gameObject.SetActive(true);
+                    SpatialMappingManager.Instance.DrawVisualMeshes = true;
+                    SpatialMappingDeformation.Instance.ResetGlobalRendering();
+                    SpatialMappingManager.Instance.StartObserver();
                 }
                 break;
             case AppState.WaitingForStageTransform:
@@ -55,7 +92,7 @@ public class AppStateManager : Singleton<AppStateManager>
                 if (HologramPlacement.Instance.GotTransform)
                 {
                     CurrentAppState = AppState.Ready;
-                    GestureManager.Instance.OverrideFocusedObject = null;
+                    GestureManager.Instance.OverrideFocusedObject = shootHandler;
                 }
                 break;
         }
